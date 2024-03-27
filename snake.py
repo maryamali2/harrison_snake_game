051effe1b8064814369618cd9de09fa9da93e2c7
maryamali2 <maryam1@seas.upenn.edu>
2024-03-27 17:25:07 -0400

    curr

--- snake.py
+++ snake.py
@@ -92,3 +92,35 @@ def show_score(choice, color, font, size):
 # Main logic
 while True:
     # TO DO: Add your main code here
+
+
+# GFX
+game_window.fill(black)
+for pos in snake_body:
+    # Snake body
+    # .draw.rect(play_surface, color, xy-coordinate)
+    # xy-coordinate -> .Rect(x, y, size_x, size_y)
+    pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
+
+
+# Snake food
+pygame.draw.rect(game_window, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
+
+
+# Game Over conditions
+# Getting out of bounds
+if snake_pos[0] < 0 or snake_pos[0] > frame_size_x-10:
+    game_over()
+if snake_pos[1] < 0 or snake_pos[1] > frame_size_y-10:
+    game_over()
+# Touching the snake body
+for block in snake_body[1:]:
+    if snake_pos[0] == block[0] and snake_pos[1] == block[1]:
+        game_over()
+
+
+show_score(1, white, 'consolas', 20)
+# Refresh game screen
+pygame.display.update()
+# Refresh rate
+fps_controller.tick(difficulty)
